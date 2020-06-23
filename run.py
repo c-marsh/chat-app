7826f2667f122e8c8fb84a48f1bf533a6c9f9b39
Chris Marsh <cmhilfiker@gmail.com>
2020-06-23 13:55:31 +0000

    add time stamps
    Signed-off-by: Chris Marsh <cmhilfiker@gmail.com>

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
 import os
-from flask import Flask
+from datetime import datetime 
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    """add messages to messages list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now, username, message))
+
+
+def get_all_messages():
+    """get all messages and separate them with  a `br`"""
+    return "<br>".join(messages)
 
 
 @app.route('/')
@@ -12,12 +25,16 @@ def index():
 
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """display chat messages"""
+    return "<h1>Welcome, {0}<h1> {1}".format(username.title(),
+                                             get_all_messages())
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """create new message and redirect to chat page"""
+    add_messages(username, message)
+    return redirect("/" + username)
 
 
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
